d8f8f820cc853efd598914d3856cfec3c8542779
leonyan1997 <leonyan5@sina.cn>
2019-03-02 20:47:59 +0800

    重新组织了下发送端的代码结构。

--- src/main/java/Source.java
+++ src/main/java/Source.java
@@ -1,9 +1,31 @@
+import jdk.nashorn.internal.ir.Block;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static config.Config.*;
 
 public class Source {
-    private byte[] getFileBytesContext(String filePath) throws IOException {
-        return Files.readAllBytes(Paths.get(filePath));
+
+    public static void main(String[] args) {
+        byte[] bytes = null;
+        try {
+            bytes = Files.readAllBytes(Paths.get(FILE_PATH));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        emit(getBlockList(bytes));
+    }
+
+    private static List<Block> getBlockList(byte[] bytes) {
+        return null;
+    }
+
+    private static void emit(List<Block> blocks) {
+        throw new Exception();
     }
 }
